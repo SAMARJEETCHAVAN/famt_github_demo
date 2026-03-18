b5dcffe42bbf4bd48ee9abd1b7b586de84fe07bf
Samarjeet Prithviraj Chavan <samarjeet@itsacheckmate.com>
2026-03-18 12:13:36 +0530

    Added a new demo file called secondfile

---
+++ secondfile.py
@@ -0,0 +1,3 @@
+print("This is second file in our demo project")
+print("This file is created to show how to create a new file and push it to github")
+print("This is a demo session for Python programming and github")
